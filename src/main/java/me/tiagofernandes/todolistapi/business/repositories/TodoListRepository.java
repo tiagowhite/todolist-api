f54b1fc2c688f45ab54521e44dab5dc6dc8b9ae3
Tiago Fernandes <tiagowhite@gmail.com>
2019-05-01 20:18:39 -0300

    Rest security token

--- src/main/java/me/tiagofernandes/todolistapi/business/repositories/TodoListRepository.java
+++ src/main/java/me/tiagofernandes/todolistapi/business/repositories/TodoListRepository.java
@@ -2,11 +2,13 @@ package me.tiagofernandes.todolistapi.business.repositories;
 
 import me.tiagofernandes.todolistapi.models.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(path = "todos", collectionResourceRel = "todos", itemResourceRel = "todo")
 public interface TodoListRepository extends MongoRepository<Todo, String> {
     Optional<Todo> findById(String id);
 }
